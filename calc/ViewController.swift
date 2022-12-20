1278639a86803381c90448e8d5834c9ffc17c406
Sabri <gitarist_sabri>
2022-12-20 22:16:43 +0300

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Sabri KOÇ on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
